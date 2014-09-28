096bfd7c8d8978acf3ea0559d87c20d45c070826
abdeljalil hh <elkoi.abdeljalil@gmail.com>
2014-09-28 18:31:09 +0100

    dd

--- index.php
+++ index.php
@@ -98,14 +98,14 @@
                 </div>
             </div>
             <div class="row">     
-                <div class="col-sm-13 portfolio-item">
+                <div class="col-sm-13 portfolio-itetmpm">
                     <a href="#portfolioModal1" class="portfolio-link " data-toggle="modal">
                         <div class="caption">
                             <div class="caption-content">
                                 <i class="fa fa-search-plus fa-3x"></i>
                             </div>
                         </div>
-                        <img src="img/portfolio/cabin.png" class="img-responsive img-centered" alt="">
+                        <img src="img/portfolio/carte.jpg" class="img-responsive img-centered" alt="">
                     </a>
                 </div>
             </div>
