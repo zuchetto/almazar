1c26da617637aa70676ce485fc5afb1f18eeac77
abdeljalil hh <elkoi.abdeljalil@gmail.com>
2014-09-29 17:35:09 +0100

    er

---
+++ index.php
@@ -0,0 +1,428 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>AlMazar - Cartes</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/mazar.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Menu</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+               <a href="#page-top"> <img src="img/icon/alamazar_logo.gif" alt="Almazar" /></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#portfolio">Notre Carte</a>
+                    </li>
+                     <li class="page-scroll">
+                        <a href="#obtenir">Obtenir La Carte</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#about">A Propos de nous</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <video width="500" height="400" controls>
+                    <source src="video/"  type="video/mp4">
+                      Your browser does not support the video tag.
+                    </video>
+                    <div class="intro-text">
+                        <span class="name">AlMazar</span>
+                        <hr class="star-primary">
+                        <span class="skills">L'esprit de Marrakech dans un mall, telle est la tonalité donnée à almazar. </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Portfolio Grid Section -->
+    <section class="carte" id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Notre Carte</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">     
+                <div class="col-sm-13 portfolio-itetmpm">
+                    <a href="#portfolioModal1" class="portfolio-link " data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/carte.jpg" class="img-responsive img-centered" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+        <!-- About Section -->
+    <section class="obtenir"  id="obtenir">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Obtenir Votre Carte WebSalaf</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="ol-lg-8 col-lg-offset-2">
+                    
+<ul class="list-inline">                   
+<li>1. Déposez votre demande de carte visa almazar dument remplie et signée 
+
+accompagnée des pièces requises au niveau de l’accueil de votre centre commercial</li>
+
+<li>2. Après acceptation de votre dossier, présentez vous à l’agence SALAFIN de Marrakech 
+
+pour signer votre contrat. Vous serez contacté par le centre commercial dés 
+
+réception de la carte visa almazar.</li>
+    </ul> 
+
+            </div>
+               
+            </div>
+                       <div class="row">
+                            <div style="center">
+                                <a target="_blank"  href="pdf/demande.pdf" >      
+                                    <button type="submit" class="btn btn-success btn-lg">Télécharger votre demande</button>
+                                </a>
+                            </div>
+                        </div>
+        </div>
+    </section>
+
+    
+    
+    <!-- About Section -->
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>A Propos de nous</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>Qui sommes-nous ? L'esprit de Marrakech dans un mall, telle est la tonalité donnée à almazar. 
+
+Un lieu ouvert sur l'extérieur, véritable compromis entre centre-ville et centre commercial classique, 
+permettant au visiteur de flâner en toute liberté, sans jamais se sentir enfermé. 
+
+Nature, Terre, Eau et Ciel, composantes distinctives de la Perle de Sud, déclinées dans le moindre 
+détail d'almazar : dans le design, dans le choix des matériaux et dans l'aménagement de l'espace. 
+
+Lumière de Marrakech et subtils éléments de tradition marocaine (fontaines, matériaux), autant de
+clins d'oeil à la ville. 100 000 m² construits 40 000 m² de surface commerciale sur 3 niveaux 100 enseignes 20 restaurants 4 Moyennes Surfaces Spécialisées 1200 places de parking Plusieurs locomotives : Hypermarché de 5000m2, Virgin Mégastore... Des zones loisirs, enfants et culture
+Best Real Estate Gestion, est développeur d'immobilier commercial. Nous concevons, réalisons, commercialisons et gérons des galeries marchandes, Label'Gallery, et des centres commerciaux.
+
+L'ambition de Best Real Estate est de se positionner comme le leader du métier de conception et gestion de centres commerciaux au Maroc.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>NOTRE MISSION:
+                        
+
+Offrir des lieux de consommation modernes dans les principales villes du Royaume, en particulier celles à potentiel industriel et/ou touristique.
+</p>
+                </div>
+                 <div class="col-lg-4">
+             <p>  NOS ATOUTS :
+                        
+
+Un concept commercial à forte notoriété articulé autour :
+
+D'une locomotive alimentaire (Label'Vie / Carrefour),
+De localisations de premier ordre,
+D'une offre variée et basée essentiellement sur des franchises de renom.
+Un partenariat avec Henry Hermand, figure emblématique des centres commerciaux en France :
+
+Pionnier dans l'histoire des centres commerciaux, Henry Hermand a accompagné leur montée en puissance en France.
+A mis son savoir-faire et sa longue expérience au service de projets communs avec Best Real Estate, dès le lancement de l'activité d'immobilier commercial (co-promoteur de la Label'Gallery de Rabat et intervenant sur plusieurs projets en cours au Maroc)</p>
+            
+            </div>
+               
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section class="contact" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Contactez Nous</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Nom</label>
+                                <input type="text" class="form-control" placeholder="Nom" id="name" required data- validation-required-message="Entrez Votre Nom Svp."  >
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Adresse Email</label>
+                                <input type="email" class="form-control" placeholder="Adresse Email" id="email" required data-validation-required-message="Entrez votre adresse email Svp.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                      
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Message</label>
+                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Entrez Votre Message Svp."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success"></div>
+                        <div class="row">
+                            <div class="form-group col-xs-12">
+                                <button type="submit" class="btn btn-success btn-lg">Envoyez</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col col-md-4">
+                        <h3>Location</h3>
+                        <p>adresse<br>marakech</p>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Notre Présence Sur Le Web</h3>
+                        <ul class="list-inline">
+                            <li>
+                                <a href="https://www.facebook.com/almazarofficiel" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>A Propos de  AlMazar</h3>
+                        <p>L'esprit de Marrakech dans un mall, telle est la tonalité donnée à almazar.  <a href="http://www.almazar.ma/">Allez sur notre site</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Copyright &copy; Almazar 2014
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll visible-xs visble-sm">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+    <!-- Portfolio Modals -->
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Carte visa almazar payer en 3, 6 ou 10 fois</h2>
+                            <hr class="star-primary">
+                            <img src="img/portfolio/carte.jpg" class="img-responsive img-centered" alt="">
+
+<h3>Bénéficiez d’une réserve d’argent permanente</h3>
+
+<p>La carte visa almazar est une réserve d’argent allant de 5 000 à 60 000dhs et qui se 
+
+reconstitue au fur et à mesure de vos remboursements. 
+
+Choisissez un mode de remboursement adapté à votre besoin
+
+Vous avez le choix entre trois modes de remboursement : 3, 6 ou 10 fois.
+
+Les remboursements seront prélevés directement de votre compte bancaire et seront 
+
+reversés sur votre carte qui sera prête à de nouvelles utilisations.
+
+Exemple 
+
+Mode de remboursement Pour un Achat de 3 000 Dhs
+
+Réglez en 3 fois (DH) 1000 (*)
+
+Réglez en 6 fois (DH) 500 (*)
+
+Réglez en 10 fois (DH) 300 (*)
+
+ (*)Les remboursements seront majorés d’intérêts calculés sur la base d’un taux effectif global (TEG) maximum 
+
+de 1,1% mensuel.
+
+Si vous n’utilisez pas votre réserve, vous n’avez aucun coût supplémentaire à régler : ni 
+
+prélèvement, ni intérêts (*) de plus vous pouvez si vous le souhaitez changer le mode de 
+
+remboursement sur simple demande à salafin.
+
+ (*) Hors frais de carte</p>
+
+<h3>Régler vos achats dans plus de 100 boutiques du centre commercial</h3>
+
+<p>Dés réception de votre carte, vous pourrez l’utiliser dans plus de 100 boutiques du centre. 
+
+La liste complète des boutiques est disponible sur <a href="www.almazar.ma">www.almazar.ma</a>
+
+Le plus de la carte ! Certaines boutiques pourront proposer le paiement en plusieurs 
+
+fois sans frais. Renseignez vous auprés de l’accueil du centre ou directement chez le 
+
+commerçant.</p>
+
+<h3>Contrôlez vos dépenses en ligne ! </h3>
+
+<p>Un site client <a href="www.salafin.com"> « www.salafin.com »</a> est mis à votre disposition pour suivre et contrôler vos 
+
+dépenses en ligne et sans se déplacer.
+
+Comment obtenir votre carte WEBSALAF
+
+1. Déposez votre demande de carte visa almazar dument remplie et signée 
+
+accompagnée des pièces requises au niveau de l’accueil de votre centre commercial
+
+2. Après acceptation de votre dossier, présentez vous à l’agence SALAFIN de Marrakech 
+
+pour signer votre contrat. Vous serez contacté par le centre commercial dés 
+
+réception de la carte visa almazar.. <a href="www.almazar.ma"> visiter le site de almazar</a> </p>
+                            
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Fermer</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    
+
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/freelancer.js"></script>
+
+</body>
+
+</html>
